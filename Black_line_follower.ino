e9fe90594db9ee384606cd71f6e4eaccacb78ea5
RAHUL VISHWAKARMA <70973839+Rocky14300@users.noreply.github.com>
2021-01-17 00:03:26 +0530

    Add files via upload

---
+++ Black_line_follower.ino
@@ -0,0 +1,68 @@
+#include "LedControl.h"
+#include "binary.h"
+LedControl lc=LedControl(7,6,5,4);
+// happy face
+byte hf[8]= {B00111100,B01000010,B10100101,B10000001,B10100101,B10011001,B01000010,B00111100};
+// neutral face
+byte nf[8]={B00111100, B01000010,B10100101,B10000001,B10111101,B10000001,B01000010,B00111100};
+// sad face
+byte sf[8]= {B00111100,B01000010,B10100101,B10000001,B10011001,B10100101,B01000010,B00111100};
+// defines pins numbers
+int l,r;                                   // initializing two variables l & r for storing inputs from Light sensors.
+void setup()
+{
+  pinMode(10,OUTPUT);                     //10,11 for left motor
+  pinMode(11,OUTPUT);
+  pinMode(12,OUTPUT);                     //12,13 for right motor
+  pinMode(13,OUTPUT);
+  pinMode(2,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(4,INPUT);                       // At pin no:4 we will connect the right light sensor which will act as an input device
+  pinMode(3,INPUT);                       // At pin no:3 we will connect the left light sensor which will act as input device.
+  digitalWrite(2,HIGH);
+  digitalWrite(5,LOW);
+}
+void loop()
+{
+  r=digitalRead(4);                       // Reading the input of right sensor and storing the value in pin no:4 of Arduino
+  l=digitalRead(3);                       // Reading the input of left sensor and storing the value in pin no:3 of Arduino
+ lc.setRow(0,0,hf[0]);
+  lc.setRow(0,1,hf[1]);
+  lc.setRow(0,2,hf[2]);
+  lc.setRow(0,3,hf[3]);
+  lc.setRow(0,4,hf[4]);
+  lc.setRow(0,5,hf[5]);
+  lc.setRow(0,6,hf[6]);
+  lc.setRow(0,7,hf[7]);
+delay(1000); 
+
+  if(r==HIGH && l==HIGH)                  // If both left & right sensor are on white surface
+  {                                       // the left & right motor will go forward
+    digitalWrite(10,HIGH);
+    digitalWrite(11,LOW);
+    digitalWrite(12,HIGH);
+    digitalWrite(13,LOW);
+  }
+  else if(l==HIGH && r==LOW)             // If left sensor is on white and right sensor is on black surface
+  {                                      // the left motor will go forward & right motor will go backward
+    digitalWrite(10,HIGH);               
+    digitalWrite(11,LOW);
+    digitalWrite(12,LOW);
+    digitalWrite(13,LOW);
+  }
+  else if(l==LOW && r==HIGH)            // If left sensor is on white and right sensor is on black surface
+  {                                     // the left motor will go backward & right motor will go forward.
+    digitalWrite(10,LOW);
+    digitalWrite(11,LOW);
+    digitalWrite(12,HIGH);
+    digitalWrite(13,LOW);
+   }
+    else                           // the last condition is, if both left & right light sensor are on black surface
+  {                                // the left & right will stop. 
+    digitalWrite(10,HIGH);
+    digitalWrite(11,HIGH);
+    digitalWrite(12,HIGH);
+    digitalWrite(13,HIGH);
+   }
+}
+    
